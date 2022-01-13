4fbfd022ae07aebc7faec32c975413a908eb8cd2
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2022-01-13 21:52:02 +0400

    tests are now passing

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(logger)
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
-//server.use('/api/users', usersRouter)
+ server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
