7021296099386e23f6d427db79631fe8dd08c1a4
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2022-01-13 07:45:34 +0400

    things to figure out

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
+
 const express = require('express');
 
-const server = express();
 const { logger} = require('./middleware/middleware')
+const usersRouter = require('./users/users-router')
+
+const server = express();
 
 server.use(express.json())
 
@@ -10,6 +13,7 @@ server.use(logger)
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+//server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require("./api/server")
+
+const Port = 9000
+
+server.listen(Port, () => {
+    console.log('listening on', Port)
+})
